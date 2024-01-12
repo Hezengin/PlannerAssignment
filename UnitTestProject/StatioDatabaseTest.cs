711ba41869169a3f81afb70d57f0e62463b0bcbe
Matheus <mateu9999@gmail.com>
2024-01-12 21:29:40 +0100

    added database unit test

---
+++ UnitTestProject/StatioDatabaseTest.cs
@@ -0,0 +1,48 @@
+﻿using PlannerAssignment.Database;
+using PlannerAssignment.Mvvm.Models;
+using PlannerAssignment.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject.Happy
+{
+    public class StatioDatabaseTest
+    {
+        [Fact]
+        public async void StationDatabaseAddTest()
+        {
+            StationDatabase stationDB = new StationDatabase(Path.Combine(Directory.GetCurrentDirectory(), "StationsDatabase.db3"));
+
+            RequestManager requestManager = new RequestManager();
+            StationModel stationModelData = await requestManager.GetUICCodeAsync("Rotterdam Centraal");
+            Station station = stationModelData.Stations[0];
+
+
+            await stationDB.SaveStationNameAsync(station);
+            Names stationNames = await stationDB.GetStationByNameAsync("Rotterdam Centraal");
+
+            Assert.NotNull(stationNames);
+            Assert.Equal("Rotterdam Centraal", stationNames.Long);
+        }
+
+        [Fact]
+        public async void StationDatabaseGetListTest()
+        {
+            StationDatabase stationDB = new StationDatabase(Path.Combine(Directory.GetCurrentDirectory(), "StationsDatabase.db3"));
+
+            RequestManager requestManager = new RequestManager();
+            StationModel stationModelData = await requestManager.GetUICCodeAsync("Rotterdam Centraal");
+            Station station = stationModelData.Stations[0];
+
+
+            await stationDB.SaveStationNameAsync(station);
+            List<Names> stationNames = await stationDB.GetAllStationsAsync();
+
+            Assert.NotNull(stationNames);
+            Assert.True(stationNames.Count > 0);
+        }
+    }
+}
